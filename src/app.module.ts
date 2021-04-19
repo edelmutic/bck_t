adf1ad42888821b14952788a831d164b723b82e4
edelmutic <edelmutic@gmail.com>
2021-04-19 21:27:51 +0300

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config"
+
 
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
             host: 'localhost',
