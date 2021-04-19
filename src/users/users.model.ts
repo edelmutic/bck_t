f17cc959649cb2cb26bdeeb48493cba909ad56fa
edelmutic <edelmutic@gmail.com>
2021-04-19 21:54:18 +0300

    add model

---
+++ src/users/users.model.ts
@@ -0,0 +1,18 @@
+import { Model } from "sequelize";
+import { Column, DataType, Table } from "sequelize-typescript";
+
+
+@Table({tableName: 'users'})
+ export class User extends Model<User> {
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    email: string;
+    @Column({type: DataType.STRING, allowNull: false})
+    password: string;
+    @Column({type: DataType.BOOLEAN, defaultValue: false})
+    banned: boolean;
+    @Column({type: DataType.STRING, allowNull: true})
+    banReason: string;
+ };
+ 
