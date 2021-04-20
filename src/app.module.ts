670f0f142f486000d65827bb740661abd714c8c4
edelmutic <edelmutic@gmail.com>
2021-04-20 20:26:40 +0300

    update modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,27 @@
-import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
+import {Module} from "@nestjs/common";
+import {SequelizeModule} from "@nestjs/sequelize";
 import { UsersModule } from './users/users.module';
-import {ConfigModule} from "@nestjs/config"
-import { User } from './users/users.model';
-
+import {ConfigModule} from "@nestjs/config";
+import {User} from "./users/users.model";
+import { RolesModule } from './roles/roles.module';
+import {Role} from "./roles/roles.model";
+import {UserRoles} from "./roles/user-roles.model";
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import {Post} from "./posts/posts.model";
+import { FilesModule } from './files/files.module';
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from 'path';
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
         ConfigModule.forRoot({
-            envFilePath: `.${process.env.NODE_ENV}.env`
+           envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        ServeStaticModule.forRoot({
+            rootPath: path.resolve( __dirname, 'static'),
         }),
         SequelizeModule.forRoot({
             dialect: 'postgres',
@@ -19,12 +30,14 @@ import { User } from './users/users.model';
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRESS_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User],
+            models: [User, Role, UserRoles, Post],
             autoLoadModels: true
-          }),
+        }),
         UsersModule,
+        RolesModule,
+        AuthModule,
+        PostsModule,
+        FilesModule,
     ]
 })
-export class AppModule {
-    
-};
+export class AppModule {}
